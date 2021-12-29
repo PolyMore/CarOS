5a7c45957a30abb71394d26988098dfa4e36aeab
Xyn <me@xyn.ro>
2021-12-29 12:14:36 +0200

    small change

--- init.sh
+++ init.sh
@@ -5,15 +5,15 @@ echo "[x] Refreshing repos"
 sudo apt update
 
 echo "[x] Installing dependencies"
-sudo apt install -y ffmpeg fswebcam
+sudo apt install -y ffmpeg fswebcam git
 
 echo "[x] Setting up fstab"
 sudo mkdir /mnt/usb
 sudo chown -R pi:pi /mnt/usb
 
 if ! grep -q 'init-usb' /etc/fstab ; then
-    echo '# init-usb' >> /etc/fstab
-    echo '/dev/sda1 /mnt/usb vfat defaults,auto,users,rw,nofail,noatime,uid=pi,gid=pi 0 0' >> /etc/fstab
+    sudo echo '# init-usb' >> /etc/fstab
+    sudo echo '/dev/sda1 /mnt/usb vfat defaults,auto,users,rw,nofail,noatime,uid=pi,gid=pi 0 0' >> /etc/fstab
 fi
 
 echo "[x] Setting up python"
